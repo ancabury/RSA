4777c59d6b415c464652b01504545ac42ffac765
Anca Sarbu <anca.sarbu93@gmail.com>
2016-01-07 00:37:00 +0200

    Calculate public and private key

--- RSA/Form1.cs
+++ RSA/Form1.cs
@@ -9,22 +9,27 @@ namespace RSA {
         PrimeNumber p;
         PrimeNumber q;
         Random random = new Random();
+        Tuple<BigInteger, BigInteger> publicKey;
+        BigInteger privateKey;
         public Form1() {
             InitializeComponent();
             p = new PrimeNumber(random);
             q = new PrimeNumber(random);
 
-            tbxMessage.Text = p.ToString();
-            tbxEncryption.Text = q.ToString();
+            tbxMessage.Text = "p = " + p.ToString();
+            tbxEncryption.Text = "q = " + q.ToString();
             generateKeys();
         }
 
         private void generateKeys() {
             BigInteger n = p.prime * q.prime;
-            BigInteger fi_n = (p.prime - 1) * (q.prime - 1);
-            BigInteger e = selectE(fi_n);
-            tbxMessage.Text += "    " + e.ToString();
-            tbxEncryption.Text += "    " + fi_n.ToString();
+            BigInteger phi_n = (p.prime - 1) * (q.prime - 1);
+            BigInteger e = selectE(phi_n);
+            publicKey = new Tuple<BigInteger, BigInteger>(n, e);
+            privateKey = modular_pow(e, -1, phi_n);
+
+            tbxMessage.Text += "    e = " + e.ToString() + "    privateKey =" + privateKey;
+            tbxEncryption.Text += "    phi(n) = " + phi_n.ToString() + "     publicKey = " + publicKey;
         }
 
         private BigInteger selectE(BigInteger fi) {
@@ -36,6 +41,17 @@ namespace RSA {
                 e = BigIntegerRandom.getRandom(size, rnd);
             } while (BigInteger.GreatestCommonDivisor(e, fi) != 1);
             return e;
+        }
+
+        BigInteger modular_pow(BigInteger value, int exponent, BigInteger modulus) {
+            BigInteger result = 1;
+            while (exponent > 0) {
+                if (exponent % 2 == 1)
+                    result = (result * value) % modulus;
+                exponent = exponent >> 1;
+                value = (value * value) % modulus;
+            }
+            return result;
         }
     }
 }
